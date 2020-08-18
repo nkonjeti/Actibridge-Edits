35de015d069a5f25b1ae9e057ac1d02bdc213ea7
Glitch (actibridge) <none>
2020-08-18 21:56:57 +0000

    💏😺 Updated with Glitch

---
+++ donate.js
@@ -0,0 +1,187 @@
+
+function popup1(){
+    var x = document.getElementById("content1");
+    x.style.display = "block";
+    var y = document.getElementById("shadow");
+    y.style.display="block";
+}
+function close1(){
+    var x = document.getElementById("content1");
+    x.style.display = "none";
+    var y = document.getElementById("shadow");
+    y.style.display="none";
+}
+
+function popup2(){
+    var x = document.getElementById("content2");
+    x.style.display = "block";
+    var y = document.getElementById("shadow");
+    y.style.display="block";
+}
+function close2(){
+    var x = document.getElementById("content2");
+    x.style.display = "none";
+    var y = document.getElementById("shadow");
+    y.style.display="none";
+}
+
+function popup3(){
+    var x = document.getElementById("content3");
+    x.style.display = "block";
+    var y = document.getElementById("shadow");
+    y.style.display="block";
+}
+function close3(){
+    var x = document.getElementById("content3");
+    x.style.display = "none";
+    var y = document.getElementById("shadow");
+    y.style.display="none";
+}
+
+function popup4(){
+    var x = document.getElementById("content4");
+    x.style.display = "block";
+    var y = document.getElementById("shadow");
+    y.style.display="block";
+}
+function close4(){
+    var x = document.getElementById("content4");
+    x.style.display = "none";
+    var y = document.getElementById("shadow");
+    y.style.display="none";
+}
+
+function popup5(){
+    var x = document.getElementById("content5");
+    x.style.display = "block";
+    var y = document.getElementById("shadow");
+    y.style.display="block";
+}
+function close5(){
+    var x = document.getElementById("content5");
+    x.style.display = "none";
+    var y = document.getElementById("shadow");
+    y.style.display="none";
+}
+
+function popup6(){
+    var x = document.getElementById("content6");
+    x.style.display = "block";
+    var y = document.getElementById("shadow");
+    y.style.display="block";
+}
+function close6(){
+    var x = document.getElementById("content6");
+    x.style.display = "none";
+    var y = document.getElementById("shadow");
+    y.style.display="none";
+}
+function popup7(){
+    var x = document.getElementById("content7");
+    x.style.display = "block";
+    var y = document.getElementById("shadow");
+    y.style.display="block";
+}
+function close7(){
+    var x = document.getElementById("content7");
+    x.style.display = "none";
+    var y = document.getElementById("shadow");
+    y.style.display="none";
+}
+function popup8(){
+    var x = document.getElementById("content8");
+    x.style.display = "block";
+    var y = document.getElementById("shadow");
+    y.style.display="block";
+}
+function close8(){
+    var x = document.getElementById("content8");
+    x.style.display = "none";
+    var y = document.getElementById("shadow");
+    y.style.display="none";
+}
+function popup9(){
+    var x = document.getElementById("content9");
+    x.style.display = "block";
+    var y = document.getElementById("shadow");
+    y.style.display="block";
+}
+function close9(){
+    var x = document.getElementById("content9");
+    x.style.display = "none";
+    var y = document.getElementById("shadow");
+    y.style.display="none";
+}
+function popup10(){
+    var x = document.getElementById("content10");
+    x.style.display = "block";
+    var y = document.getElementById("shadow");
+    y.style.display="block";
+}
+function close10(){
+    var x = document.getElementById("content10");
+    x.style.display = "none";
+    var y = document.getElementById("shadow");
+    y.style.display="none";
+}
+function popup11(){
+    var x = document.getElementById("content11");
+    x.style.display = "block";
+    var y = document.getElementById("shadow");
+    y.style.display="block";
+}
+function close11(){
+    var x = document.getElementById("content11");
+    x.style.display = "none";
+    var y = document.getElementById("shadow");
+    y.style.display="none";
+}
+function popup12(){
+    var x = document.getElementById("content12");
+    x.style.display = "block";
+    var y = document.getElementById("shadow");
+    y.style.display="block";
+}
+function close12(){
+    var x = document.getElementById("content12");
+    x.style.display = "none";
+    var y = document.getElementById("shadow");
+    y.style.display="none";
+}
+function popup13(){
+    var x = document.getElementById("content13");
+    x.style.display = "block";
+    var y = document.getElementById("shadow");
+    y.style.display="block";
+}
+function close13(){
+    var x = document.getElementById("content13");
+    x.style.display = "none";
+    var y = document.getElementById("shadow");
+    y.style.display="none";
+}
+function popup14(){
+    var x = document.getElementById("content14");
+    x.style.display = "block";
+    var y = document.getElementById("shadow");
+    y.style.display="block";
+}
+function close14(){
+    var x = document.getElementById("content14");
+    x.style.display = "none";
+    var y = document.getElementById("shadow");
+    y.style.display="none";
+}
+function popup15(){
+    var x = document.getElementById("content15");
+    x.style.display = "block";
+    var y = document.getElementById("shadow");
+    y.style.display="block";
+}
+function close15(){
+    var x = document.getElementById("content15");
+    x.style.display = "none";
+    var y = document.getElementById("shadow");
+    y.style.display="none";
+}
+
